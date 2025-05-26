e555dee500ae797b12e3a9022614270221b98c8d
Valmir Júnior <valmirgmj@gmail.com>
2025-05-26 17:32:54 -0300

    Implement my default testes account as part of the setup script

--- scripts/ResetMongoDB.ts
+++ scripts/ResetMongoDB.ts
@@ -17,10 +17,23 @@ async function resetMongoDB() {
 
         for (const collection of collections) {
             const result = await collection.deleteMany({});
+
             console.log(
                 `Cleared ${collection.collectionName}: ${result.deletedCount} documents removed.`,
             );
         }
+
+        await db.collection('credentials').insertOne({
+            email: 'valmirgmj@gmail.com',
+            password: 'a',
+        });
+
+        await db.collection('users').insertOne({
+            fullname: 'Valmir G. Martins Júnior',
+            email: 'valmirgmj@gmail.com',
+            birthdate: '2000-04-13',
+            nickname: 'Val',
+        });
 
         console.log('All collections cleared successfully.');
     } catch (error) {
