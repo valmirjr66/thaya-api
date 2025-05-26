cf563d13be0011a44e2bbf9e2b66872ab370ae28
Valmir Júnior <valmirgmj@gmail.com>
2025-05-26 17:14:39 -0300

    Implement authentication mechanism

--- src/user.module.ts
+++ src/user.module.ts
@@ -3,6 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import UserController from './modules/user/UserController';
 import UserService from './modules/user/UserService';
 
+import {
+    Credential,
+    CredentialSchema,
+} from './modules/user/schemas/CredentialSchema';
 import { User, UserSchema } from './modules/user/schemas/UserSchema';
 
 @Module({
@@ -10,6 +14,9 @@ import { User, UserSchema } from './modules/user/schemas/UserSchema';
     providers: [UserService],
     imports: [
         MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+        MongooseModule.forFeature([
+            { name: Credential.name, schema: CredentialSchema },
+        ]),
     ],
 })
 export class UserModule {}
