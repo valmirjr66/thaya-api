aa6f2faffbebc9fb4f453ac30c94be92d0a85259
valmir-duetto <valmir.junior-c@duettoresearch.com>
2025-09-07 22:24:25 -0300

    remove unused import

--- src/routines.module.ts
+++ src/routines.module.ts
@@ -1,16 +1,11 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import RoutinesController from './modules/routines/RoutinesController';
 import RoutinesService from './modules/routines/RoutinesService';
-import { User, UserSchema } from './modules/user/schemas/UserSchema';
-import UserService from './modules/user/UserService';
 import CalendarService from './modules/user/CalendarService';
+import UserService from './modules/user/UserService';
 
 @Module({
     controllers: [RoutinesController],
     providers: [RoutinesService, UserService, CalendarService],
-    imports: [
-        MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-    ],
 })
 export class RoutinesModule {}
