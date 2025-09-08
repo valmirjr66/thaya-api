fc70dadf3ad7b7abb6a24c7ec625c7fec09ca0a4
valmir-duetto <valmir.junior-c@duettoresearch.com>
2025-09-07 22:50:34 -0300

    fix modules imports

--- src/routines.module.ts
+++ src/routines.module.ts
@@ -1,11 +1,38 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import BlobStorageManager from './handlers/cloud/BlobStorageManager';
+import TelegramHandler from './handlers/messaging/TelegramHandler';
 import RoutinesController from './modules/routines/RoutinesController';
 import RoutinesService from './modules/routines/RoutinesService';
 import CalendarService from './modules/user/CalendarService';
+import {
+    Calendar,
+    CalendarSchema,
+} from './modules/user/schemas/CalendarSchema';
+import {
+    Credential,
+    CredentialSchema,
+} from './modules/user/schemas/CredentialSchema';
+import { User, UserSchema } from './modules/user/schemas/UserSchema';
 import UserService from './modules/user/UserService';
 
 @Module({
     controllers: [RoutinesController],
-    providers: [RoutinesService, UserService, CalendarService],
+    providers: [
+        RoutinesService,
+        UserService,
+        CalendarService,
+        TelegramHandler,
+        BlobStorageManager,
+    ],
+    imports: [
+        MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+        MongooseModule.forFeature([
+            { name: Credential.name, schema: CredentialSchema },
+        ]),
+        MongooseModule.forFeature([
+            { name: Calendar.name, schema: CalendarSchema },
+        ]),
+    ],
 })
 export class RoutinesModule {}
--- src/telegram.module.ts
+++ src/telegram.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import BlobStorageManager from './handlers/cloud/BlobStorageManager';
+import CalendarTool from './handlers/gpt/CalendarTool';
+import ChatAssistant from './handlers/gpt/ChatAssistant';
+import UserInfoTool from './handlers/gpt/UserInfoTool';
+import WeatherTool from './handlers/gpt/WeatherTool';
 import TelegramHandler from './handlers/messaging/TelegramHandler';
 import AssistantService from './modules/assistant/AssistantService';
+import { Chat, ChatSchema } from './modules/assistant/schemas/ChatSchema';
+import {
+    FileMetadata,
+    FileMetadataSchema,
+} from './modules/assistant/schemas/FileMetadataSchema';
+import {
+    Message,
+    MessageSchema,
+} from './modules/assistant/schemas/MessageSchema';
 import TelegramController from './modules/telegram/TelegramController';
 import TelegramService from './modules/telegram/TelegramService';
+import {
+    Credential,
+    CredentialSchema,
+} from './modules/user/schemas/CredentialSchema';
+import { User, UserSchema } from './modules/user/schemas/UserSchema';
 import UserService from './modules/user/UserService';
 
 @Module({
@@ -12,6 +32,24 @@ import UserService from './modules/user/UserService';
         TelegramHandler,
         UserService,
         AssistantService,
+        BlobStorageManager,
+        ChatAssistant,
+        WeatherTool,
+        UserInfoTool,
+        CalendarTool,
+    ],
+    imports: [
+        MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+        MongooseModule.forFeature([
+            { name: Credential.name, schema: CredentialSchema },
+        ]),
+        MongooseModule.forFeature([
+            { name: Message.name, schema: MessageSchema },
+        ]),
+        MongooseModule.forFeature([{ name: Chat.name, schema: ChatSchema }]),
+        MongooseModule.forFeature([
+            { name: FileMetadata.name, schema: FileMetadataSchema },
+        ]),
     ],
 })
 export class TelegramModule {}
