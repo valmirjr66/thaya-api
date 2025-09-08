d8035577bd8775d2be7299f9cef3810fa2efa292
valmir-duetto <valmir.junior-c@duettoresearch.com>
2025-09-07 21:49:39 -0300

    replace message handling strategy

--- src/telegram.module.ts
+++ src/telegram.module.ts
@@ -3,10 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import TelegramController from './modules/telegram/TelegramController';
 import TelegramService from './modules/telegram/TelegramService';
 import { User, UserSchema } from './modules/user/schemas/UserSchema';
+import TelegramHandler from './handlers/messaging/TelegramHandler';
 
 @Module({
     controllers: [TelegramController],
-    providers: [TelegramService],
+    providers: [TelegramService, TelegramHandler],
     imports: [
         MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     ],
