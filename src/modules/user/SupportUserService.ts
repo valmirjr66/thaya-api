15c935a2985c5f6ac3f22e3eafa91629d20f2c1c
valmir-duetto <valmir.junior-c@duettoresearch.com>
2025-09-19 14:07:50 -0300

    fix(user): handle potential null value when fetching user info by id

--- src/modules/user/SupportUserService.ts
+++ src/modules/user/SupportUserService.ts
@@ -52,7 +52,7 @@ export default class SupportUserService {
             const user = await this.userModel
                 .findById(new mongoose.Types.ObjectId(id))
                 .exec()
-                .then((doc) => doc.toObject());
+                .then((doc) => doc?.toObject());
 
             if (!user) {
                 this.logger.error(`User with id ${id} not found`);
