3d81319e41a618829ba52ecef45d510a06e931ef
valmir-duetto <valmir.junior-c@duettoresearch.com>
2025-09-19 18:48:55 -0300

    fix(AuthenticateUserRequestDto): enforce required properties for email and password

--- src/modules/user/dto/AuthenticateUserRequestDto.ts
+++ src/modules/user/dto/AuthenticateUserRequestDto.ts
@@ -1,10 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export default class AuthenticateUserRequestDto {
-    @ApiProperty()
+    @ApiProperty({ required: true })
     public email: string;
 
-    @ApiProperty()
+    @ApiProperty({ required: true })
     public password: string;
 
     constructor(email: string, password: string) {
